261f3009e5698530cb6885ed03799dda4a09a82b
Vishnu <54411695+devish99@users.noreply.github.com>
2020-06-08 20:03:21 +0530

    Extract Phone Numbers and Email IDs from a file
    1) Copy text from a file
    2) Run the program
    3) Phone Numbers and Email IDs are automatically copied to the clipboard
    NOTE: Install "pip install pyperclip". 
    Cannot match "+" and parenthesis.

---
+++ Phone_Email_Scraper.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import re
+import pyperclip
+# Create a regex object for phone numbers
+phoneRegex = re.compile(r''' 
+# +91 9876543210 +91 98765 43210  020 87654321 020 8765 4321  020-87654321 (020) 8765 4321                       
+(
+((\+\d\d)|(\d\d))?         #+91 or 91
+((\d\d\d)|(\(\d\d\d\)))?  #020 or (020)
+(\s|-)#hyphen or space separator
+(\d{8,10})|(\d{4,5}\s\d{4,5}) #phone number mobile or landline
+)
+''', re.VERBOSE)
+
+# Create a regex object for email addresses
+emailRegex = re.compile(r'''
+#some.+_thing@(\d{2,5}))?.com
+
+[a-zA-Z0-9_.+] +            #name part
+@                           #@symbol
+[a-zA-Z0-9_.+]+             #domain name part
+
+''', re.VERBOSE)
+
+# Get text from the clipboard
+text = pyperclip.paste()
+
+# Extract phone and email from the text
+extractedPhone = phoneRegex.findall(text)
+extractedEmail = emailRegex.findall(text)
+
+allPhoneNumbers = []
+for phoneNumber in extractedPhone:
+    allPhoneNumbers.append(phoneNumber[0])
+
+    
+print (allPhoneNumbers)
+print (extractedEmail)
+
+# Copy the extracted phone and email to the clipboard
+results = '\n'.join(allPhoneNumbers)+'\n'+'\n'.join(extractedEmail)
+pyperclip.copy(results)
